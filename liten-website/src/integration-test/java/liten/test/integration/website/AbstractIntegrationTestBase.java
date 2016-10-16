c48f8cc04e01fffce30c689040196936da415437
Steven <vstrength@gmail.com>
2016-10-16 00:08:08 -0700

    Adding integration test

--- liten-website/src/integration-test/java/liten/test/integration/website/AbstractIntegrationTestBase.java
+++ liten-website/src/integration-test/java/liten/test/integration/website/AbstractIntegrationTestBase.java
@@ -8,14 +8,18 @@ import org.eclipse.jetty.server.Server;
 import org.junit.AfterClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.env.PropertySource;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.File;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Alexander Shabanov
@@ -57,17 +61,32 @@ public abstract class AbstractIntegrationTestBase {
     LOG.info("Server stopped");
   }
 
-  protected static void initServer(@Nonnull String springConfig) {
+  protected static void initServer(@Nullable SimpleServiceUser user, @Nonnull String springConfig) {
+    StandardLauncher.ensureLoggersConfigured();
+
     THREAD = new Thread(() -> {
+      // TODO: replace with TempConfiguration
       try {
         final String props = StandardLauncher.CONFIG_KEY_PORT + "=" + PORT_NUMBER + "\n" +
             StandardLauncher.CONFIG_KEY_SHUTDOWN_DELAY + "=100\n" +
             "\n";
 
-        final File tmpFile = File.createTempFile("litenIntegrationTest", ".properties");
+        final File tmpFile = File.createTempFile("brikar-website-integration-test", ".properties");
         tmpFile.deleteOnExit();
         Files.write(Paths.get(tmpFile.toURI()), props.getBytes(StandardCharsets.UTF_8));
 
+        System.setProperty(StandardLauncher.SYS_PROP_SETTINGS_OVERRIDE, tmpFile.toURI().toURL().toExternalForm());
+
+        // start server
+        try (final StandardLauncher launcher = new StandardLauncher(springConfig) {
+          @Override
+          protected void setServerSettings(@Nonnull Server server) {
+            super.setServerSettings(server);
+            SERVER = server;
+          }
+        }) {
+          launcher.start();
+        }
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
@@ -76,7 +95,12 @@ public abstract class AbstractIntegrationTestBase {
     THREAD.start();
     LOG.info("Server started");
 
-//    ServerApiUtil.waitUntilStarted(user, getServerUrl("/rest"));
+    ServerApiUtil.waitUntilStarted(user, getServerUrl("/rest"));
     LOG.info("Server initialized");
+  }
+
+  @Nonnull
+  protected static URI getServerUrl(String relPath) {
+    return URI.create("http://127.0.0.1:" + PORT_NUMBER + relPath);
   }
 }
--- liten-website/src/integration-test/java/liten/test/integration/website/RestControllerTest.java
+++ liten-website/src/integration-test/java/liten/test/integration/website/RestControllerTest.java
@@ -1,11 +1,17 @@
 package liten.test.integration.website;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * @author Alexander Shabanov
  */
-public class RestControllerTest {
+public class RestControllerTest extends AbstractIntegrationTestBase {
+
+  @BeforeClass
+  public static void initServer() {
+    initServer(null, "classpath:/litenIntegrationTest/");
+  }
 
   @Test
   public void shouldPass() {
