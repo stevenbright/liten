b2ac5c28df66d28f9a658d8ffd646ebad1cfe751
Steven <vstrength@gmail.com>
2016-11-20 23:22:07 -0800

    Adding working test

--- liten-website/src/integration-test/java/liten/test/integration/website/RestControllerTest.java
+++ liten-website/src/integration-test/java/liten/test/integration/website/RestControllerTest.java
@@ -1,12 +1,21 @@
 package liten.test.integration.website;
 
+import com.truward.brikar.client.rest.RestOperationsFactory;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestOperations;
+
+import java.net.URI;
+
+import static org.junit.Assert.assertTrue;
+import static org.springframework.web.util.UriComponentsBuilder.fromUri;
 
 /**
  * @author Alexander Shabanov
  */
-public class RestControllerTest extends AbstractIntegrationTestBase {
+public final class RestControllerTest extends AbstractIntegrationTestBase {
 
   @BeforeClass
   public static void initServer() {
@@ -14,7 +23,17 @@ public class RestControllerTest extends AbstractIntegrationTestBase {
   }
 
   @Test
-  public void shouldPass() {
-    //....
+  public void shouldGetPublicPages() {
+    final URI baseUri = getServerUrl("/");
+
+    try (final RestOperationsFactory rof = new RestOperationsFactory(new StringHttpMessageConverter())) {
+      final RestOperations ro = rof.getRestOperations();
+
+      final URI uri = fromUri(baseUri).pathSegment("g").pathSegment("index").build().toUri();
+      final ResponseEntity<String> entity = ro.getForEntity(uri, String.class);
+      final String body = entity.getBody();
+
+      assertTrue(body.length() > 0);
+    }
   }
 }
