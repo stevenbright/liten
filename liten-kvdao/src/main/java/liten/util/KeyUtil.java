e8f251b010aab8426c02a652940ff7c96edbcb5a
Steven <vstrength@gmail.com>
2017-03-26 22:37:44 -0700

    KeyUtil - TBD

--- liten-kvdao/src/main/java/liten/util/KeyUtil.java
+++ liten-kvdao/src/main/java/liten/util/KeyUtil.java
@@ -10,6 +10,9 @@ public final class KeyUtil {
   private KeyUtil() {}
 
   public static int getKeyBodyIndex(String serviceName, String entityName, int minVersion, int maxVersion) {
+    //IdCodec codec = SemanticIdCodec.forService(serviceName).withEntityName(entityName);
+    //codec.decodeLong()
+
     throw new UnsupportedOperationException();
   }
 }
