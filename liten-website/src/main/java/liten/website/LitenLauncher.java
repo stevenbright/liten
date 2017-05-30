1ace717f13b0cce577fd0e6f67bee66e64068d90
Steven <vstrength@gmail.com>
2017-05-30 13:02:41 -0700

    Use persistent sessions to avoid re-login on app quit

--- liten-website/src/main/java/liten/website/LitenLauncher.java
+++ liten-website/src/main/java/liten/website/LitenLauncher.java
@@ -2,9 +2,16 @@ package liten.website;
 
 import com.truward.brikar.server.launcher.StandardLauncher;
 import com.truward.demo.media.DemoMediaServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.session.HashSessionIdManager;
+import org.eclipse.jetty.server.session.HashSessionManager;
+import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
 import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 
 /**
  * @author Alexander Shabanov
@@ -17,6 +24,27 @@ public final class LitenLauncher {
       protected void initServlets(@Nonnull ServletContextHandler contextHandler) {
         super.initServlets(contextHandler);
         contextHandler.addServlet(DemoMediaServlet.class, "/demo/media/*");
+      }
+
+      @Override
+      protected void initContextFilters(@Nonnull ServletContextHandler contextHandler) {
+        super.initContextFilters(contextHandler);
+
+        // persist sessions on the disk to avoid noisy reload problem
+        final HashSessionManager sessionManager = new HashSessionManager();
+        try {
+          // TODO: make configurable
+          final File sessionDir = new File("/tmp/liten");
+          if (!sessionDir.exists() && !sessionDir.mkdir()) {
+            throw new IOException("Unable to create session dir");
+          }
+          sessionManager.saveSessions(true);
+          sessionManager.setStoreDirectory(sessionDir);
+        } catch (Exception e) {
+          throw new IllegalStateException(e);
+        }
+        final SessionHandler sessionHandler = new SessionHandler(sessionManager);
+        contextHandler.setSessionHandler(sessionHandler);
       }
     }) {
       launcher
