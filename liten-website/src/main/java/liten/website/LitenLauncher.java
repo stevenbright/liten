25dfe2ea44d1282ffe4917ba9eebd055aa552f2d
Steven <vstrength@gmail.com>
2017-06-04 23:22:20 -0700

    Introduce persistent session dir property

--- liten-website/src/main/java/liten/website/LitenLauncher.java
+++ liten-website/src/main/java/liten/website/LitenLauncher.java
@@ -9,11 +9,13 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * @author Alexander Shabanov
  */
 public final class LitenLauncher {
+  private static final String SESSION_DIR_PROP_NAME = "liten.web.sessionDir";
 
   public static void main(String[] args) throws Exception {
     try (StandardLauncher launcher = new StandardLauncher("classpath:/litenWebsite/") {
@@ -30,11 +32,12 @@ public final class LitenLauncher {
         // persist sessions on the disk to avoid noisy reload problem
         final HashSessionManager sessionManager = new HashSessionManager();
         try {
-          // TODO: make configurable
-          final File sessionDir = new File("/tmp/liten");
+          final String sessionDirPath = this.getPropertyResolver().getProperty(SESSION_DIR_PROP_NAME);
+          final File sessionDir = new File(Objects.requireNonNull(sessionDirPath, SESSION_DIR_PROP_NAME));
           if (!sessionDir.exists() && !sessionDir.mkdir()) {
             throw new IOException("Unable to create session dir");
           }
+
           sessionManager.saveSessions(true);
           sessionManager.setStoreDirectory(sessionDir);
         } catch (Exception e) {
