ce78cd4404002855285a04b7add4c88fe6764602
Steven <vstrength@gmail.com>
2017-11-03 12:25:02 -0700

    Remove duplication of test fixture data

--- liten-kvdao/src/test/java/liten/catalog/test/IseCatalogIngestionTest.java
+++ liten-kvdao/src/test/java/liten/catalog/test/IseCatalogIngestionTest.java
@@ -1,11 +1,9 @@
 package liten.catalog.test;
 
-import com.truward.time.UtcTime;
 import liten.catalog.dao.IseCatalogDao;
 import liten.catalog.dao.support.DefaultIseCatalogDao;
 import liten.catalog.dao.support.IseCatalogIngestionHelper;
 import liten.catalog.model.Ise;
-import liten.catalog.util.IseNames;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,13 +11,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+import static liten.catalog.dao.support.IseCatalogSampleData.createItemList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
@@ -38,7 +34,7 @@ public final class IseCatalogIngestionTest extends XodusTestBase {
 
   @Test
   public void shouldExportData() throws IOException {
-    final List<Ise.Item> items = createTestItemList();
+    final List<Ise.Item> items = createItemList();
 
     environment.executeInTransaction(tx -> {
       for (int i = 0; i < items.size(); ++i) {
@@ -76,205 +72,6 @@ public final class IseCatalogIngestionTest extends XodusTestBase {
       qb.setCursor(res.getCursor());
     }
 
-    assertEquals(createTestItemList().size(), items.size());
-  }
-
-  //
-  // Private
-  //
-
-  private static List<Ise.Item> createTestItemList() {
-    return Arrays.asList(
-        Ise.Item.newBuilder().setId("ci1-X010").setType("language")
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("English"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Английский"))
-            .addExternalIds(Ise.ExternalId.newBuilder().setIdType("alias").setIdValue("en"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-X020").setType("language")
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Russian"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Русский"))
-            .addExternalIds(Ise.ExternalId.newBuilder().setIdType("alias").setIdValue("ru"))
-            .build(),
-
-        //
-        // Genres
-        //
-
-        Ise.Item.newBuilder().setId("ci1-G010").setType(IseNames.GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Novel"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Новелла"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-G030").setType(IseNames.GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Fantasy"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-G040").setType(IseNames.GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Detective"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-G050").setType(IseNames.GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Sci-Fi"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-G070").setType(IseNames.GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Religion"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Религия"))
-            .build(),
-
-        //
-        // Authors
-        //
-
-        Ise.Item.newBuilder().setId("ci1-A010").setType(IseNames.PERSON)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Leo Tolstoy"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Лев Николаевич Толстой"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-A040").setType(IseNames.PERSON)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Arkady Strugatsky"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Аркадий Стругацкий"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-A050").setType(IseNames.PERSON)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Boris Strugatsky"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Борис Стругацкий"))
-            .build(),
-
-        //
-        // Books
-        //
-
-        Ise.Item.newBuilder().setId("ci1-B010").setType(IseNames.BOOK)
-            .setExtras(Ise.ItemExtras.newBuilder().setBook(Ise.BookItemExtras.newBuilder()
-                .addGenreIds("ci1-G070")
-                .build()))
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Holy Bible"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Библия"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-B210").setType(IseNames.BOOK)
-            .setExtras(Ise.ItemExtras.newBuilder().setBook(Ise.BookItemExtras.newBuilder()
-                .addGenreIds("ci1-G010")
-                .addAuthorIds("ci1-A010")
-                .build()))
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("War and Peace")
-                .addEntries(Ise.Entry.newBuilder()
-                    .setCreatedTimestamp(date("2012-09-24").getTime())
-                    .setDownloadInfo(Ise.DownloadInfo.newBuilder()
-                        .setDownloadType("librus")
-                        .setDownloadId("1013")
-                        .setOriginId("15")
-                        .setFileSize(1213453)
-                        .build())
-                    .build()))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Война и мир")
-                .addEntries(Ise.Entry.newBuilder()
-                    .setCreatedTimestamp(date("2012-11-05").getTime())
-                    .setDownloadInfo(Ise.DownloadInfo.newBuilder()
-                        .setDownloadType("librus")
-                        .setDownloadId("1011")
-                        .setOriginId("15")
-                        .setFileSize(1532145)
-                        .build())
-                    .build()))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-B250").setType(IseNames.BOOK)
-            .setExtras(Ise.ItemExtras.newBuilder().setBook(Ise.BookItemExtras.newBuilder()
-                .addGenreIds("ci1-G010").addGenreIds("ci1-G050")
-                .addAuthorIds("ci1-A040").addAuthorIds("ci1-A050")
-                .build()))
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("The Land of Crimson Clouds")
-                .addEntries(Ise.Entry.newBuilder()
-                    .setCreatedTimestamp(date("2012-11-02").getTime())
-                    .setDownloadInfo(Ise.DownloadInfo.newBuilder()
-                        .setDownloadType("librus")
-                        .setDownloadId("1024")
-                        .setOriginId("15")
-                        .setFileSize(111250)
-                        .build())
-                    .build()))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Страна багровых туч")
-                .addEntries(Ise.Entry.newBuilder()
-                    .setCreatedTimestamp(date("2012-10-24").getTime())
-                    .setDownloadInfo(Ise.DownloadInfo.newBuilder()
-                        .setDownloadType("librus")
-                        .setDownloadId("1025")
-                        .setOriginId("15")
-                        .setFileSize(125460)
-                        .build())
-                    .build()))
-            .build(),
-
-        strugatskySciFiNovel("Отель «У Погибшего Альпиниста»", "Dead Mountaineer's Hotel"),
-        strugatskySciFiNovel("Малыш", "Space Mowgli"),
-        strugatskySciFiNovel("Пикник на обочине", "Roadside Picnic"),
-        strugatskySciFiNovel("За миллиард лет до конца света", "Definitely Maybe"),
-        strugatskySciFiNovel("Град обреченный", "The Doomed City"),
-        strugatskySciFiNovel("Повесть о дружбе и недружбе", "Tale of Friendship and Non-friendship"),
-        strugatskySciFiNovel("Жук в муравейнике", "Beetle in the Anthill"),
-        strugatskySciFiNovel("Хромая судьба", "Limping Fate"),
-        strugatskySciFiNovel("Волны гасят ветер", "The Time Wanderers"),
-        strugatskySciFiNovel("Отягощённые злом", "Overburdened with Evil"),
-        strugatskySciFiNovel("Второе нашествие марсиан", "The Second Invasion from Mars"),
-        strugatskySciFiNovel("Сказка о Тройке", "Tale of the Troika"),
-        strugatskySciFiNovel("Обитаемый остров", "Prisoners of Power"),
-        strugatskySciFiNovel("Беспокойство", "Disquiet"),
-        strugatskySciFiNovel("Улитка на склоне", "Snail on the Slope"),
-        strugatskySciFiNovel("Попытка к бегству", "Escape Attempt"),
-        strugatskySciFiNovel("Далёкая Радуга", "Far Rainbow"),
-        strugatskySciFiNovel("Трудно быть богом", "Hard to Be a God"),
-        strugatskySciFiNovel("Понедельник начинается в субботу", "Monday Begins on Saturday"),
-        strugatskySciFiNovel("Хищные вещи века", "The Final Circle of Paradise"),
-        strugatskySciFiNovel("Извне", "From Beyond"),
-        strugatskySciFiNovel("Путь на Амальтею", "Destination: Amaltheia"),
-        strugatskySciFiNovel("Полдень, XXII век", "Noon: 22nd Century"),
-        strugatskySciFiNovel("Стажеры", "Space Apprentice"),
-        strugatskySciFiNovel("Гадкие лебеди", "The Ugly Swans"),
-
-        sampleBook("Test 4"),
-        sampleBook("Test 5"),
-        sampleBook("Test 6"),
-        sampleBook("Test 7"),
-        sampleBook("Test 8"),
-
-        //
-        // Unused (sample)
-        //
-
-        Ise.Item.newBuilder().setId("ci1-G990").setType(IseNames.GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("philosophy"))
-            .build()
-
-    );
-  }
-
-  private static Ise.Item strugatskySciFiNovel(String ruTitle, String enTitle) {
-    return Ise.Item.newBuilder().setType(IseNames.BOOK)
-        .setExtras(Ise.ItemExtras.newBuilder().setBook(Ise.BookItemExtras.newBuilder()
-            .addGenreIds("ci1-G010").addGenreIds("ci1-G050")
-            .addAuthorIds("ci1-A040").addAuthorIds("ci1-A050")
-            .build()))
-        .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle(enTitle))
-        .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle(ruTitle))
-        .build();
-  }
-
-  private static Ise.Item sampleBook(String enTitle) {
-    return Ise.Item.newBuilder().setType(IseNames.BOOK)
-        .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle(enTitle))
-        .build();
-  }
-
-  private static UtcTime date(String strDate) {
-    final SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-    fmt.setTimeZone(UtcTime.newUtcTimeZone());
-    try {
-      return UtcTime.valueOf(fmt.parse(strDate).getTime());
-    } catch (ParseException e) {
-      throw new IllegalArgumentException(e);
-    }
+    assertEquals(createItemList().size(), items.size());
   }
 }
--- liten-website/src/main/java/liten/website/service/init/DemoInitializer.java
+++ liten-website/src/main/java/liten/website/service/init/DemoInitializer.java
@@ -1,20 +1,13 @@
 package liten.website.service.init;
 
-import com.truward.time.UtcTime;
 import liten.catalog.dao.IseCatalogDao;
+import liten.catalog.dao.support.IseCatalogSampleData;
 import liten.catalog.model.Ise;
-import liten.catalog.util.IseNames;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.List;
-
-import static liten.catalog.util.IseNames.*;
 
 /**
  * Developer mode data initializer.
@@ -31,216 +24,11 @@ public final class DemoInitializer {
     log.info("Initializing demo data");
 
     catalogDao.getEnvironment().executeInTransaction(tx -> {
-      for (final Ise.Item item : createTestItemList()) {
+      for (final Ise.Item item : IseCatalogSampleData.createItemList()) {
         catalogDao.persist(tx, item);
       }
     });
 
     log.info("Demo data added");
-  }
-
-  private static List<Ise.Item> createTestItemList() {
-    return Arrays.asList(
-
-        //
-        // Languages
-        //
-
-        Ise.Item.newBuilder().setId("ci1-x010").setType("language")
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("English"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Английский"))
-            .addExternalIds(IseNames.newAlias("en")) // alias:en
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-x020").setType("language")
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Russian"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Русский"))
-            .addExternalIds(IseNames.newAlias("ru")) // alias:ru
-            .build(),
-
-        //
-        // Genres
-        //
-
-        Ise.Item.newBuilder().setId("ci1-g010").setType(GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Novel"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Новелла"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-g030").setType(GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Fantasy"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-g040").setType(GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Detective"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-g050").setType(GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Sci-Fi"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-g070").setType(GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Religion"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Религия"))
-            .build(),
-
-        //
-        // Authors
-        //
-
-        Ise.Item.newBuilder().setId("ci1-a010").setType(PERSON)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Leo Tolstoy"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Лев Николаевич Толстой"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-a040").setType(PERSON)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Arkady Strugatsky"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Аркадий Стругацкий"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-a050").setType(PERSON)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Boris Strugatsky"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Борис Стругацкий"))
-            .build(),
-
-        //
-        // Books
-        //
-
-        Ise.Item.newBuilder().setId("ci1-b010").setType(BOOK)
-            .setExtras(Ise.ItemExtras.newBuilder().setBook(Ise.BookItemExtras.newBuilder()
-                .addGenreIds("ci1-g070")
-                .build()))
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("Holy Bible"))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Библия"))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-b210").setType(BOOK)
-            .setExtras(Ise.ItemExtras.newBuilder().setBook(Ise.BookItemExtras.newBuilder()
-                .addGenreIds("ci1-g010")
-                .addAuthorIds("ci1-a010")
-                .build()))
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("War and Peace")
-                .addEntries(Ise.Entry.newBuilder()
-                    .setId("1")
-                    .setCreatedTimestamp(date("2012-09-24").getTime())
-                    .setDownloadInfo(Ise.DownloadInfo.newBuilder()
-                        .setDownloadType("librus")
-                        .setDownloadId("1013")
-                        .setOriginId("15")
-                        .setFileSize(1213453)
-                        .build())
-                    .build())
-                .addEntries(Ise.Entry.newBuilder()
-                    .setId("2")
-                    .setCreatedTimestamp(date("2012-09-25").getTime())
-                    .build()))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Война и мир")
-                .addEntries(Ise.Entry.newBuilder()
-                    .setCreatedTimestamp(date("2012-11-05").getTime())
-                    .setDownloadInfo(Ise.DownloadInfo.newBuilder()
-                        .setDownloadType("librus")
-                        .setDownloadId("1011")
-                        .setOriginId("15")
-                        .setFileSize(1532145)
-                        .build())
-                    .build()))
-            .build(),
-
-        Ise.Item.newBuilder().setId("ci1-B250").setType(BOOK)
-            .setExtras(Ise.ItemExtras.newBuilder().setBook(Ise.BookItemExtras.newBuilder()
-                .addGenreIds("ci1-g010").addGenreIds("ci1-g050")
-                .addAuthorIds("ci1-a040").addAuthorIds("ci1-a050")
-                .build()))
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("The Land of Crimson Clouds")
-                .addEntries(Ise.Entry.newBuilder()
-                    .setCreatedTimestamp(date("2012-11-02").getTime())
-                    .setDownloadInfo(Ise.DownloadInfo.newBuilder()
-                        .setDownloadType("librus")
-                        .setDownloadId("1024")
-                        .setOriginId("15")
-                        .setFileSize(111250)
-                        .build())
-                    .build()))
-            .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle("Страна багровых туч")
-                .addEntries(Ise.Entry.newBuilder()
-                    .setCreatedTimestamp(date("2012-10-24").getTime())
-                    .setDownloadInfo(Ise.DownloadInfo.newBuilder()
-                        .setDownloadType("librus")
-                        .setDownloadId("1025")
-                        .setOriginId("15")
-                        .setFileSize(125460)
-                        .build())
-                    .build()))
-            .build(),
-
-        strugatskySciFiNovel("Отель «У Погибшего Альпиниста»", "Dead Mountaineer's Hotel"),
-        strugatskySciFiNovel("Малыш", "Space Mowgli"),
-        strugatskySciFiNovel("Пикник на обочине", "Roadside Picnic"),
-        strugatskySciFiNovel("За миллиард лет до конца света", "Definitely Maybe"),
-        strugatskySciFiNovel("Град обреченный", "The Doomed City"),
-        strugatskySciFiNovel("Повесть о дружбе и недружбе", "Tale of Friendship and Non-friendship"),
-        strugatskySciFiNovel("Жук в муравейнике", "Beetle in the Anthill"),
-        strugatskySciFiNovel("Хромая судьба", "Limping Fate"),
-        strugatskySciFiNovel("Волны гасят ветер", "The Time Wanderers"),
-        strugatskySciFiNovel("Отягощённые злом", "Overburdened with Evil"),
-        strugatskySciFiNovel("Второе нашествие марсиан", "The Second Invasion from Mars"),
-        strugatskySciFiNovel("Сказка о Тройке", "Tale of the Troika"),
-        strugatskySciFiNovel("Обитаемый остров", "Prisoners of Power"),
-        strugatskySciFiNovel("Беспокойство", "Disquiet"),
-        strugatskySciFiNovel("Улитка на склоне", "Snail on the Slope"),
-        strugatskySciFiNovel("Попытка к бегству", "Escape Attempt"),
-        strugatskySciFiNovel("Далёкая Радуга", "Far Rainbow"),
-        strugatskySciFiNovel("Трудно быть богом", "Hard to Be a God"),
-        strugatskySciFiNovel("Понедельник начинается в субботу", "Monday Begins on Saturday"),
-        strugatskySciFiNovel("Хищные вещи века", "The Final Circle of Paradise"),
-        strugatskySciFiNovel("Извне", "From Beyond"),
-        strugatskySciFiNovel("Путь на Амальтею", "Destination: Amaltheia"),
-        strugatskySciFiNovel("Полдень, XXII век", "Noon: 22nd Century"),
-        strugatskySciFiNovel("Стажеры", "Space Apprentice"),
-        strugatskySciFiNovel("Гадкие лебеди", "The Ugly Swans"),
-
-        sampleBook("Test 4"),
-        sampleBook("Test 5"),
-        sampleBook("Test 6"),
-        sampleBook("Test 7"),
-        sampleBook("Test 8"),
-
-        //
-        // Unused (sample)
-        //
-
-        Ise.Item.newBuilder().setId("ci1-g990").setType(GENRE)
-            .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle("philosophy"))
-            .build()
-
-    );
-  }
-
-  private static Ise.Item strugatskySciFiNovel(String ruTitle, String enTitle) {
-    return Ise.Item.newBuilder().setType(BOOK)
-        .setExtras(Ise.ItemExtras.newBuilder().setBook(Ise.BookItemExtras.newBuilder()
-            .addGenreIds("ci1-g010").addGenreIds("ci1-g050")
-            .addAuthorIds("ci1-a040").addAuthorIds("ci1-a050")
-            .build()))
-        .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle(enTitle))
-        .addSkus(Ise.Sku.newBuilder().setId("2").setLanguage("ru").setTitle(ruTitle))
-        .build();
-  }
-
-  private static Ise.Item sampleBook(String enTitle) {
-    return Ise.Item.newBuilder().setType(BOOK)
-        .addSkus(Ise.Sku.newBuilder().setId("1").setLanguage("en").setTitle(enTitle))
-        .build();
-  }
-
-  private static UtcTime date(String strDate) {
-    final SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-    fmt.setTimeZone(UtcTime.newUtcTimeZone());
-    try {
-      return UtcTime.valueOf(fmt.parse(strDate).getTime());
-    } catch (ParseException e) {
-      throw new IllegalArgumentException(e);
-    }
   }
 }
